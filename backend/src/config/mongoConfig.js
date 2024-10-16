0432a0c3fac76c2337b0bdedd97773881a50e32a
Dev Patel <88072755+THE-DGP@users.noreply.github.com>
2024-10-16 07:44:26 +0530

    Update mongoConfig.js

--- backend/src/config/mongoConfig.js
+++ backend/src/config/mongoConfig.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-require('dotenv').config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      useCreateIndex: true
     });
     console.log('MongoDB Connected...');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
+  } catch (err) {
+    console.error('MongoDB Connection Error:', err);
     process.exit(1); // Exit process with failure
   }
 };
