0529740c5262fc6822d6559daa0ea6c56bf3eb47
Dev Patel <88072755+THE-DGP@users.noreply.github.com>
2024-10-17 11:59:56 +0530

    Update mongoConfig.js

--- backend/src/config/mongoConfig.js
+++ backend/src/config/mongoConfig.js
@@ -1,23 +1,17 @@
 // src/config/mongoConfig.js
+
 const { MongoClient } = require('mongodb');
 
 let client;
 
 const connectDB = async () => {
-  if (client) {
-    // Reuse the existing client if already connected
-    return client;
-  }
+  if (client) return client; // Reuse the existing client
 
   try {
     const uri = process.env.MONGO_URI;
-    client = new MongoClient(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      maxPoolSize: 10, // Allow multiple concurrent requests (adjust as needed)
-    });
+    client = new MongoClient(uri, { useNewUrlParser: true });
     await client.connect();
-    console.log("MongoDB connected successfully");
+    console.log("MongoDB connected successfully.");
     return client;
   } catch (error) {
     console.error("Failed to connect to MongoDB:", error);
